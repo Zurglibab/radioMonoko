822083b9ad23dff6dcaa9474da3cb9c83346e5f4
Theo Rousseau <trousseau370@gmail.com>
2026-05-18 13:19:39 +0200

    Test push

--- radioMonoco_Web/src/components/header/NavBar.tsx
+++ radioMonoco_Web/src/components/header/NavBar.tsx
@@ -244,7 +244,7 @@ const NavBar = () => {
                                             onClick={handleLogout}
                                             className="w-full flex items-center gap-3 px-4 py-2 text-sm text-primary hover:bg-primary/10 transition-colors text-left cursor-pointer outline-none font-bold"
                                         >
-                                            <HiOutlineLogout className="text-lg" /> Déconnexion
+                                            <HiOutlineLogout className="text-lg" /> Déconnexiona
                                         </button>
                                     </div>
                                 )}
